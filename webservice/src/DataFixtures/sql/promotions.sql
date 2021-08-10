9ee3e68740d70c80809ed4a7a87aa340c2f23f51
Carlos Blanco Gañán <carlos.blanga@gmail.com>
2021-08-10 19:46:22 +0200

    feat: assign values to brand-new columns of table named promotion
    Load promotions into DB with values for its brand-new columns
    category_name and
    product_sku

--- webservice/src/DataFixtures/sql/promotions.sql
+++ webservice/src/DataFixtures/sql/promotions.sql
@@ -1,5 +1,5 @@
 USE `promotions-assigment`;
-insert into `promotion` (`id`, `discount_percentage`, `description`) values 
-(uuid(), '30.00', "Apply to products in the 'boots' category"),
-(uuid(), '15.00', "Apply to product whose 'sku' is 000003")
+insert into `promotion` (`id`, `discount_percentage`, `description`, `category_name`, `product_sku`) values 
+(uuid(), '30.00', "Apply to products in the 'boots' category", 'boots', null),
+(uuid(), '15.00', "Apply to product whose 'sku' is 000003", null, '000003')
 ;
