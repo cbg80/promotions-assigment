bbabb635d141ac48432ca627cd930d782b1e639e
Carlos Blanco Gañán <carlos.blanga@gmail.com>
2021-08-20 01:14:06 +0200

    feat: query products and discounts and get them filtered and paginated
    Declare the interface named ProductRepository with the described
    behaviour and
    make the class named DoctrineProductRepository implement it.
    Make use of the dependency inversion principle, aka hexagonal
    architecture

--- webservice/src/Repository/adapters/DoctrineProductRepository.php
+++ webservice/src/Repository/adapters/DoctrineProductRepository.php
@@ -7,6 +7,7 @@ namespace App\Repository\adapters;
 use App\Entity\Product;
 use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use App\Repository\ports\ProductRepository;
 
 /**
  * @method Product|null find($id, $lockMode = null, $lockVersion = null)
@@ -14,11 +15,41 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Product[]    findAll()
  * @method Product[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class DoctrineProductRepository extends ServiceEntityRepository
+class DoctrineProductRepository extends ServiceEntityRepository implements ProductRepository
 {
+    const FIND_LIMIT = 5;
+    
     public function __construct(ManagerRegistry $registry)
     {
         parent::__construct($registry, Product::class);
+    }
+    
+    public function findWithoutDiscountApplied(int $findOffset, ?string $categoryNameFilter = NULL, int $priceOriginalLessThanFilter = 0): ?array
+    {
+        $productQueryBuilder = $this->createQueryBuilder('p')
+            ->select('p')
+            ->addSelect('MAX(ProductPromotion.discountPercentage)')
+            ->addSelect('MAX(CategoryPromotion.discountPercentage)')
+            ->leftJoin('p.promotions', 'ProductPromotion')
+            ->leftJoin('p.categories', 'c')
+            ->leftJoin('c.promotions', 'CategoryPromotion')
+            ->groupBy('p.id')
+            ->setFirstResult($findOffset)
+            ->setMaxResults(self::FIND_LIMIT)
+        ;
+        
+        if (isset($categoryNameFilter))
+            $productQueryBuilder = $productQueryBuilder->andWhere('c.name LIKE :categoryName')
+                ->setParameter('categoryName', $categoryNameFilter)
+            ;
+        if (!empty($priceOriginalLessThanFilter))
+            $productQueryBuilder = $productQueryBuilder->andWhere('p.price.original <= :priceOriginalLessThan')
+                ->setParameter('priceOriginalLessThan', $priceOriginalLessThanFilter, \PDO::PARAM_INT)
+            ;
+        
+        return $productQueryBuilder->getQuery()
+            ->getResult()
+        ;
     }
 
     // /**
